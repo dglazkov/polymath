dglazkov/polymath

commit dcd5b4d5d51e1fccb90063c6ced7663927099d30

When converting an OG library to a new one, make sure not to overshadow any entries.
The OG format allowed a list of embeddings, multiple of which might share the same issue_id. Before this
commit, only the last embedding for a given issue_id would be saved.

Now, if we detect a collision we make a new chunk_id by appending a counter until we find an unused one.

Part of #12.

ask_embeddings.py
@@ -103,9 +103,17 @@ def _convert_library_from_version_og(og_library):
     library = empty_library()
     for embedding in og_library['embeddings']:
         text, embedding, token_count, issue_id = embedding
+
+        #Multiple embedding rows might have the same issue_id, so append a
+        #counter if necessary to not overshadow any items.
+        chunk_id = issue_id
+        count = 0
+        while chunk_id in library['content']:
+            chunk_id = issue_id + '_' + str(count)
+            count += 1
+
         url, image_url, title, description = og_library['issue_info'].get(issue_id, ('', '', '', ''))
-        #TODO: handle cases where the OG file has multiple embedding lines with the same content.
-        library['content'][issue_id] = {
+        library['content'][chunk_id] = {
             'text': text,
             'embedding': embedding,
             'token_count': token_count,

commit 59fd90f9e505e91971e784351bf161f7add01626

_convert_library_from_version_og handles old files with an integer issue_id.
Previously those would cause an error.

Bug was initially introduced in dcd5b4d5d51e1fccb90063c6ced7663927099d30.

Part of #12.

ask_embeddings.py
@@ -120,10 +120,10 @@ def _convert_library_from_version_og(og_library):
 
         #Multiple embedding rows might have the same issue_id, so append a
         #counter if necessary to not overshadow any items.
-        chunk_id = issue_id
+        chunk_id = str(issue_id)
         count = 0
         while chunk_id in library['content']:
-            chunk_id = issue_id + '_' + str(count)
+            chunk_id = str(issue_id) + '_' + str(count)
             count += 1
 
         url, image_url, title, description = og_library['issue_info'].get(issue_id, ('', '', '', ''))