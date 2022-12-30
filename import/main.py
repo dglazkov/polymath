18cb59df4b8f985f15464aec529f2af9f18b9abb
Alex Komoroske <jkomoros@gmail.com>
2022-12-30 11:17:28 -0800

    Pop nakedlibrary out into a separate file.
    Part of #1.

--- import/main.py
+++ import/main.py
@@ -2,41 +2,10 @@ import ask_embeddings
 
 import argparse
 import json
-import sys
 import pickle
 import os
 
-class NakedLibraryImporter:
-    def get_chunks(self, filename, existing_library, max_lines = -1):
-        # Will return a dict of chunks, possibly missing embedding and token_count.
-        data = ask_embeddings.load_data_file(filename)
-
-        chunks = data.get('content')
-        if not chunks:
-            raise Exception('Data did not have content as expected')
-        count = 0
-        total = len(chunks) if max_lines < 0 else max_lines
-        result = {}
-        for id, chunk in chunks.items():
-            if max_lines >= 0 and count >= max_lines:
-                print('Reached max lines')
-                break
-            if id in existing_library['content']:
-                continue
-            print(f'Processing new chunk {id} ({count + 1}/{total})')
-            text = chunk.get('text')
-            if not text:
-                print('Skipping a row with id ' + id + ' that was missing text')
-                continue
-            result[id] = {
-                    'text': text,
-                    'info': chunk.get('info')
-            }
-            count += 1
-        return result
-    def output_base_filename(self, input_filename):
-        base_filename, file_extension = os.path.splitext(input_filename)
-        return base_filename
+from .nakedlibrary import NakedLibraryImporter
 
 IMPORTERS = {
     'library': NakedLibraryImporter()
