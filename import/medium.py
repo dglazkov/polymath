0c7cdb6c14b174b0b423fa06e76755693c498517
Alex Komoroske <jkomoros@gmail.com>
2022-12-30 15:44:32 -0800

    Medium exporter correctly extracts the main url.
    Currently it just prints it.
    
    Part of #9.

--- import/medium.py
+++ import/medium.py
@@ -4,9 +4,6 @@ from bs4 import BeautifulSoup
 
 
 class MediumImporter:
-    def get_filenames(self, path: str) -> list:
-        return glob.glob(f"{path}/posts/*.html")
-
     def output_base_filename(self, filename):
         profile_path = f"{filename}/profile/profile.html"
         with open(profile_path, "r") as f:
@@ -15,7 +12,22 @@ class MediumImporter:
             username = ele.get_text(strip=True)
             return username.replace('@','')
 
+
+    def extract_url_from_soup(self, base_filename : str, soup : BeautifulSoup):
+        if (base_filename.startswith('draft_')):
+            footer = soup.find('footer')
+            ele = footer.find('a')
+        else:
+            ele = soup.find('a', class_='p-canonical')
+        return ele.get('href')
+
+
     def get_chunks(self, filename):
-        #TODO: implement
-        print('This importer is not yet implemented')
-        raise StopIteration
+        filenames = glob.glob(f"{filename}/posts/*.html")
+        for file in filenames:
+            with open(file, 'r') as f:
+                base_filename = os.path.basename(file)
+                soup = BeautifulSoup(f, "html.parser")
+                url = self.extract_url_from_soup(base_filename, soup)
+                print(url)
+        print('Actual importing not yet implemented')
