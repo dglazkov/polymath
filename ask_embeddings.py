dglazkov/polymath

commit c8002308e8875934eee6c58a08d2cbb66d325171

Make sure that import.main uses b64, too.

import/main.py
@@ -75,7 +75,7 @@ for id, chunk in importer.get_chunks(filename):
     text = strip_emoji(chunk.get('text', ''))
     if 'embedding' not in chunk:
         print(f'Fetching embedding for {id}')
-        chunk['embedding'] = ask_embeddings.get_embedding(text)
+        chunk['embedding'] = ask_embeddings.base64_from_vector(ask_embeddings.get_embedding(text)).decode("ascii") 
     if 'token_count' not in chunk:
         print(f'Fetching token_count for {id}')
         chunk['token_count'] = ask_embeddings.get_token_count(text)

commit 330733fa2411b7f8e3907803d0a5757908e9c438

Fixed a bug where `import.main` would mangle files who didn't need any embeddings set.
Libraries should always have emeddings as an array of floats while in memory.

Broke in c800230.

Part of #24.

ask_embeddings.py
@@ -168,6 +168,11 @@ def embeddings_to_arrays(library):
         chunk['embedding'] = vector_from_base64(chunk['embedding'])
 
 
+def arrays_to_embeddings(library):
+    for _, chunk in library['content'].items():
+        chunk['embedding'] = base64_from_vector(chunk['embedding']).decode('ascii')
+
+
 def load_library(library_file):
     library = load_data_file(library_file)
     version = library.get('version', -1)

import/main.py
@@ -94,8 +94,7 @@ for id, chunk in importer.get_chunks(filename):
     text = strip_emoji(chunk.get('text', ''))
     if 'embedding' not in chunk:
         print(f'Fetching embedding for {id}')
-        chunk['embedding'] = ask_embeddings.base64_from_vector(
-            ask_embeddings.get_embedding(text)).decode("ascii")
+        chunk['embedding'] = ask_embeddings.get_embedding(text)
     if 'token_count' not in chunk:
         print(f'Fetching token_count for {id}')
         chunk['token_count'] = ask_embeddings.get_token_count(text)
@@ -106,6 +105,8 @@ print(f'Loaded {count} new lines')
 
 if not os.path.exists(ask_embeddings.LIBRARY_DIR):
     os.mkdir(ask_embeddings.LIBRARY_DIR)
+
+ask_embeddings.arrays_to_embeddings(result)
 
 if output_format == 'json':
     with open(full_output_filename, 'w') as f: