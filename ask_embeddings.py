dglazkov/polymath

commit cb62d50d00738c40e2aaac39de2cbc7a3c146752

Remove legacy get_chunks in ask_embeddings.py.
Part of #15.

ask_embeddings.py
@@ -336,10 +336,6 @@ def get_context(chunk_ids, library, count=MAX_CONTEXT_LEN_IN_TOKENS, count_type_
     return result
 
 
-def get_chunks(chunk_ids, library):
-    return [library['content'][chunk_id]['info'] for chunk_id in chunk_ids]
-
-
 def get_context_for_library(library : Library):
     """
     Returns an array of all text for every chunk in library
@@ -501,5 +497,5 @@ def ask(query, context_query=None, library_file=None):
     context = list(context_dict.values())
     chunk_ids = list(context_dict.keys())
 
-    chunks = get_chunks(chunk_ids, library)
-    return get_completion_with_context(query, context), chunks
+    infos = [library.chunk(chunk_id)['info'] for chunk_id in chunk_ids]
+    return get_completion_with_context(query, context), infos

commit 2da9199defdca51d5afa780bfd2975e66d861dbe

Oops, fix a bug broken in cb62d50d00738c40e2aaac39de2cbc7a3c146752.
We were using legacy library objects but trying to call a method on them.

Part of #15.

ask_embeddings.py
@@ -474,14 +474,16 @@ def get_completion_with_context(query, context):
 def ask(query, context_query=None, library_file=None):
     if not context_query:
         context_query = query
-    library = load_library(
+    data = load_library(
         library_file) if library_file else load_default_libraries()
     query_embedding = get_embedding(context_query)
-    similiarities_dict = get_similarities(query_embedding, library)
-    context_dict = get_context(similiarities_dict.keys(), library)
+    similiarities_dict = get_similarities(query_embedding, data)
+    context_dict = get_context(similiarities_dict.keys(), data)
 
     context = list(context_dict.values())
     chunk_ids = list(context_dict.keys())
+
+    library = Library(data=data)
 
     infos = [library.chunk(chunk_id)['info'] for chunk_id in chunk_ids]
     return get_completion_with_context(query, context), infos