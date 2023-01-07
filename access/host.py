0fe349dfce1ba7200d8b2823703255e1fb393c9d
Alex Komoroske <jkomoros@gmail.com>
2023-01-07 15:15:39 -0800

    Allow specifying the file to modify in access.host
    Part of #26.

--- access/host.py
+++ access/host.py
@@ -8,9 +8,6 @@ import re
 
 DEFAULT_ACCESS_FILE = 'access.SECRET.json'
 
-# TODO: allow this to be specified
-access_file = DEFAULT_ACCESS_FILE
-
 
 def generate_token_for_user(user_id):
     base = base64.urlsafe_b64encode(secrets.token_bytes(16)).decode('utf-8')
@@ -22,6 +19,7 @@ def save_access_file(data, access_file=DEFAULT_ACCESS_FILE):
     with open(access_file, 'w') as f:
         json.dump(data, f, indent='\t')
     print(f"Don't forget to redeploy with the updated {access_file}")
+
 
 def add_token_for_user(user_id, access_file=DEFAULT_ACCESS_FILE, force=False):
     token = generate_token_for_user(user_id)
@@ -49,11 +47,13 @@ parser.add_argument("command", help="The command to run", choices=['add'],
                     default='add')
 parser.add_argument("user_id", help="The id of the user to modify")
 parser.add_argument("--force", help="If true, will add a new token even if one already exists", action="store_true")
+parser.add_argument("--file", help="The access file to operate on", default=DEFAULT_ACCESS_FILE)
 args = parser.parse_args()
 
 command = args.command
 user_id = args.user_id
 force = args.force
+file = args.file
 
 if command == 'add':
-    add_token_for_user(user_id, access_file=access_file, force=force)
+    add_token_for_user(user_id, access_file=file, force=force)
