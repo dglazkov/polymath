dglazkov/polymath

commit 8eabcd6fbf71ffb7350cb823c2eda1d95dd7b833

access.host add --> grant
Part of #26.

access/host.py
@@ -69,8 +69,8 @@ def revoke_token_for_user(user_id, access_file=DEFAULT_ACCESS_FILE, force=False)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("command", help="The command to run", choices=['add', 'revoke'],
-                    default='add')
+parser.add_argument("command", help="The command to run", choices=['grant', 'revoke'],
+                    default='grant')
 parser.add_argument("user_id", help="The id of the user to modify")
 parser.add_argument("--force", help="Whether to force the action", action="store_true")
 parser.add_argument("--file", help="The access file to operate on", default=DEFAULT_ACCESS_FILE)

commit 25c6eb264843ef5b1e78363bf14401740fd239f8

Fix a bug broken in 8eabcd6fbf71ffb7350cb823c2eda1d95dd7b833 where the command wouldn't do anything with `grant`.
Part of #26.

access/host.py
@@ -81,7 +81,9 @@ user_id = args.user_id
 force = args.force
 file = args.file
 
-if command == 'add':
+if command == 'grant':
     add_token_for_user(user_id, access_file=file, force=force)
 elif command == 'revoke':
     revoke_token_for_user(user_id, access_file=file, force=force)
+else:
+    print(f'Unknown command {command}')