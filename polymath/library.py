dglazkov/polymath

commit 8ce20f9a94a4f050b76e0eb22d79dcc2782847ef

Make it so setting Library.omit will go through and trim items that currently exist to ensure they match the new value.
Part of #52.

polymath/library.py
@@ -209,7 +209,14 @@ class Library:
     @omit.setter
     def omit(self, value):
         _, _, canonical_value = _keys_to_omit(value)
+        if canonical_value == self._data['omit']:
+            return
         self._data['omit'] = canonical_value
+        if self.omit_whole_chunk:
+            self._data['content'] = {}
+            return
+        for chunk_id, chunk in self.chunks:
+            self.set_chunk(chunk_id, chunk)
 
     @property
     def seed(self):

commit 412d329c50927f0404fd74f537daa30853b4438d

Fix a bug introduced in 8ce20f9a94a4f050b76e0eb22d79dcc2782847ef where setting omit might fail.
Part of #52.

polymath/library.py
@@ -209,7 +209,7 @@ class Library:
     @omit.setter
     def omit(self, value):
         _, _, canonical_value = _keys_to_omit(value)
-        if canonical_value == self._data['omit']:
+        if 'omit' in self._data and canonical_value == self._data['omit']:
             return
         self._data['omit'] = canonical_value
         if self.omit_whole_chunk: