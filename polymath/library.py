20ee6bbc61d98fa9d2d1875d6d5a4ceb65cbbe0c
Alex Komoroske <jkomoros@gmail.com>
2023-01-16 14:42:03 -0800

    Fix a bug in _re_sort where we iterated through the wrong list of chunks when sorting for similarities.
    Part of #52.

--- polymath/library.py
+++ polymath/library.py
@@ -294,7 +294,7 @@ class Library:
         elif sort_type == 'similarity':
             ids_to_sort = []
             # TODO: use a list comprehension or whatever
-            for chunk_id, chunk in self.chunks:
+            for chunk_id, chunk in self._data['content'].items():
                 similarity = chunk.get('similarity', None)
                 if similarity == None:
                     raise Exception(f'sort of similarity passed but {chunk_id} had no similarity')
