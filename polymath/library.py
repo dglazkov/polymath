dglazkov/polymath

commit dc024e7a53ce079795a92e554867eae94db678c2

Make it so _re_sort will print a failed assertion earlier around a list of ids and content that are out of sync.
Part of #52.

polymath/library.py
@@ -296,7 +296,10 @@ class Library:
         elif sort_type == 'similarity':
             ids_to_sort = []
             # TODO: use a list comprehension or whatever
-            for chunk_id, chunk in self._data['content'].items():
+            for chunk_id in ids:
+                chunk = self.chunk(id)
+                if not chunk:
+                    raise Exception(f'similarity sort started with a chunk that no longer exists: {chunk_id}')
                 similarity = chunk.get('similarity', None)
                 if similarity == None:
                     raise Exception(f'sort of similarity passed but {chunk_id} had no similarity')

commit 58ffb7e7b6ee6cc72dfea9fa282eb3e0c3cc5f2e

Fixed a bug introduced in dc024e7a53ce079795a92e554867eae94db678c2 where no chunks would have their similarity fetched.
Part of #52.

polymath/library.py
@@ -301,7 +301,7 @@ class Library:
             ids_to_sort = []
             # TODO: use a list comprehension or whatever
             for chunk_id in ids:
-                chunk = self.chunk(id)
+                chunk = self.chunk(chunk_id)
                 if not chunk:
                     raise Exception(f'similarity sort started with a chunk that no longer exists: {chunk_id}')
                 similarity = chunk.get('similarity', None)