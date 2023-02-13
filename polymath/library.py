dglazkov/polymath

commit b1a50342ed6398de9e0e46c0054e58785d4f8aed

Wire through more explicit NDArray types.
This MIGHT have broken pinecone indexing.

Part of #101.

polymath/library.py
@@ -57,11 +57,11 @@ def canonical_id(bit_text, url=''):
 # where `data` is an array of floats
 
 
-def vector_from_base64(str):
+def vector_from_base64(str) -> NDArray[np.float32]:
     return np.frombuffer(base64.b64decode(str), dtype=np.float32)
 
 
-def vector_similarity(x, y):
+def vector_similarity(x: NDArray[np.float32], y: NDArray[np.float32]) -> float:
     # np.dot returns a float32 but those aren't serializable in json. Just
     # covert to a float64 now.
     return float(np.dot(np.array(x), np.array(y)))
@@ -747,11 +747,12 @@ class Library:
         with open(filename, 'w') as f:
             json.dump(result, f, indent='\t')
 
-    def _similarities(self, query_embedding):
+    def _similarities(self, query_embedding : NDArray[np.float32]):
         bits = sorted([
             (vector_similarity(query_embedding, bit.embedding), bit.id)
             for bit
-            in self.bits], reverse=True)
+            in self.bits
+            if bit.embedding], reverse=True)
         return {key: value for value, key in bits}
 
     def compute_similarities(self, query_embedding):
@@ -812,7 +813,7 @@ class Library:
             'access_token': access_token
         })
 
-    def _produce_query_result(self, query_embedding : List[float]):
+    def _produce_query_result(self, query_embedding : NDArray[np.float32]):
         self.compute_similarities(query_embedding)
         self.sort = 'similarity'
 

polymath/pinecone.py
@@ -35,7 +35,7 @@ class PineconeLibrary(Library):
             namespace=self.config.namespace,
             top_k=TOP_K,
             include_metadata=True,
-            vector=query_embedding
+            vector=query_embedding.tolist()
         )
         for item in result['matches']:
             bit = Bit(data={

commit ded91cf457329efa3cc3fd3bed574e7c9575510b

Fix an error introduced in b1a50342ed6398de9e0e46c0054e58785d4f8aed
Part of #101.# Please enter the commit message for your changes. Lines starting

polymath/library.py
@@ -752,7 +752,7 @@ class Library:
             (vector_similarity(query_embedding, bit.embedding), bit.id)
             for bit
             in self.bits
-            if bit.embedding], reverse=True)
+            if bit.embedding is not None], reverse=True)
         return {key: value for value, key in bits}
 
     def compute_similarities(self, query_embedding):