6a4722d60ef09d3b4fb420a2859acf3ee3185497
Alex Komoroske <jkomoros@gmail.com>
2023-02-26 08:15:00 -0800

    sitemap importer (which already inherits from HTMLScraperImporter -> BaseImporter) uses override hints.
    Part of #101.

--- convert/sitemap.py
+++ convert/sitemap.py
@@ -4,15 +4,20 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from argparse import Namespace
 
+from .base import GetChunksResult
+
+from overrides import override
 
 class SitemapImporter(HTMLScraperImporter):
     def __init__(self):
       self._debug = False
 
+    @override
     def retrieve_arguments(self, args: Namespace):
         self._debug = args.debug
 
-    def get_chunks(self, filename):
+    @override
+    def get_chunks(self, filename) -> GetChunksResult:
       # Fetch the sitemap (need to validate this is a sitemap)
       r = requests.get(filename)
       soup = BeautifulSoup(r.text, "html.parser")
@@ -26,7 +31,8 @@ class SitemapImporter(HTMLScraperImporter):
         for chunk in super().get_chunks(url):
           yield chunk
     
-    def output_base_filename(self, url):
-        url_parts = urlparse(url)
+    @override
+    def output_base_filename(self, filename) -> str:
+        url_parts = urlparse(filename)
         path = url_parts.path.replace('/', '-')
         return 'sitemap-%s%s' % (url_parts.hostname, path)
