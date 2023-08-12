48bc108c6e97352a281296ff012c4e93b82d7d9a
Erika RS <erika.rice.scherpelz@gmail.com>
2023-08-12 12:03:41 -0700

    Revert "Change similarity value from int to float"
    This reverts commit aa335d8bb2d6efac4f750e09ef78e1acfc6f3e17.

--- polymath/library.py
+++ polymath/library.py
@@ -253,7 +253,7 @@ class Bit:
 
     @property
     def similarity(self) -> float:
-        result = self._data.get('similarity', -1.0)
+        result = self._data.get('similarity', -1)
         if not isinstance(result, float):
             raise Exception('similarity not float as expected')
         return result
